10d3c36ed85eedfb49fa83396d1ff6888396f876
Maya Hagan <miyaclessa@gmail.com>
2022-03-02 22:44:31 +0000

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import UserList from "./Components/UserList";
+import ClientForms from "./Components/ClientForms";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+  handleAddUser = (newUser) => {
+    newUser.id = Math.random().toString();
+    this.setState({
+      users: [...this.state.users, newUser],
+    });
+  };
+
+  handleDeleteUser = (userId) => {
+    const newArr = this.state.users.filter((user) => {
+      return user.id !== userId;
+    });
+    this.setState({ users: newArr });
+  };
+  handleEditUser = (updatedUser) => {
+    this.setState({
+      users: this.state.users.map((user) =>
+        user.id === updatedUser.id ? updatedUser : user
+      ),
+    });
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <ClientForms addUser={this.handleAddUser} />
+
+              <div className="details col-md-3">
+                <UserList
+                  users={this.state.users}
+                  deleteUser={this.handleDeleteUser}
+                  editUser={this.handleEditUser}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
